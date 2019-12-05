eecf8b2f496c088aca0388c6d49f88b0a9050390
YvieSkye <57970260+YvieSkye@users.noreply.github.com>
2019-12-05 13:10:48 +1300

    Add files via upload

---
+++ CannonController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+
+///    public bool isFiring;
+
+    public BulletController bullet;
+    public float bulletSpeed;
+    public float timeBetweenShots;
+    private float shotCounter;
+    private bool CannonisFiring;
+
+    public Transform firePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            FireCannon();
+        }
+
+//        if (Input.GetMouseButtonUp(0))
+//            CannonisFiring = false;
+
+     }
+
+
+    void FireCannon()
+    {
+        shotCounter -= Time.deltaTime;
+        {
+            if (shotCounter < -0)
+            shotCounter = timeBetweenShots;
+            BulletController newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation) as BulletController;
+            newBullet.speed = bulletSpeed;
+        }
+
+    }
+}
