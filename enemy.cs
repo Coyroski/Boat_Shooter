73b7096cbaca77755f5797208f77f057b3a42574
YvieSkye <57970260+YvieSkye@users.noreply.github.com>
2019-12-09 13:27:12 +1300

    Add files via upload

---
+++ enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed;
+
+    private int current;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);   
+            //print("WOrking");
+           // print("working02");
+        }
+        else
+            nexttarget();
+    }
+     void nexttarget()
+    {
+        current = (current + 1) % target.Length;
+        Debug.Log(current);
+    }
+
+}
